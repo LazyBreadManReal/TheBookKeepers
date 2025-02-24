49b8dbb3d2ab81b56f39e74e027621159ca83338
= <61348311+HotDogMastah@users.noreply.github.com>
2025-02-24 12:32:43 +0800

    implemented homepage ui

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -2,9 +2,39 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "../assets/css/HomePage.css";
 import goatImage from "../assets/images/goats/goat.png";
+import farmImage from "/src/assets/images/farm-bg.jpg"
+
+
+interface Farm {
+  imageUrl: string;
+  name: string;
+  animalCount: number;
+  location: string;
+}
 
 const HomePage: React.FC = () => {
   const [userData, setUserData] = useState(null);
+  
+  const farms: Farm[] = [
+    {
+      imageUrl: "/src/assets/images/goats/goat.png", 
+      name: "John Doe Farm",
+      animalCount: 15,
+      location: "Huntington Beach, CA",
+    },
+    {
+      imageUrl: "/src/assets/images/goats/goat.png", 
+      name: "John Doe Farm",
+      animalCount: 15,
+      location: "Huntington Beach, CA",
+    },
+    {
+      imageUrl: "/src/assets/images/goats/goat.png", 
+      name: "John Doe Farm",
+      animalCount: 15,
+      location: "Huntington Beach, CA",
+    },
+  ];
 
   useEffect(() => {
     const fetchProtectedData = async () => {
@@ -36,7 +66,7 @@ const HomePage: React.FC = () => {
     <div className="content">
       <div className="intro-banner">
         <div className="intro-banner-gradient"></div>
-        <img src={goatImage} alt="goatimage" className="intro-banner-image" />
+        <img src="/src/assets/images/goats/goat.png" alt="goatimage" className="intro-banner-image" />
         <div className="intro-banner-content">
           <p className="company-stand">Insert Company stance</p>
           <button className="become-a-member-button">Become A Member</button>
@@ -62,13 +92,34 @@ const HomePage: React.FC = () => {
                 Stay updated on the latest news, special offers, and exclusive
                 insights. Get valuable updates delivered straight to your inbox.
               </p>
-              <button className="signup-btn">
+              <button className="signup-btn-home">
                 Sign Up <span className="arrow">→</span>
               </button>
             </div>
           ))}
         </div>
       </section>
+      <div className="sponsored-farms-banner">
+        <div className="sponsored-banner-gradient"></div>
+        <img src="/src/assets/images/farm-bg.jpg" alt="farm image" className="sponsored-banner-image" />
+        <div className="sponsored-banner-content">
+          <p>Sponsored Farms</p>
+        </div>
+      </div>
+      <div className="farm-list">
+        {farms.map((farm, index) => (
+          <div key={index} className="farm-card">
+            <img src={farm.imageUrl} alt={farm.name} className="farm-image" />
+            <div className="farm-info">
+              <h3 className="farm-name">{farm.name}</h3>
+              <p className="animal-count">
+                <span className="bold-text">{farm.animalCount} Goats</span>
+              </p>
+              <p className="farm-location">{farm.location}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
